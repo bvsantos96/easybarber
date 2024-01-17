3ed1d82a3e498f30bfdd09973041fe1c6ad73479
Filipe Santos <filipesantosdev@gmail.com>
2024-01-17 21:09:03 +0000

    feat: Updated paths to use the defined @babel.config.js

--- Client/babel.config.js
+++ Client/babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      development: {
+        plugins: [
+          ['module-resolver', {
+            root: ['./'],
+            alias: {
+              "@components": "./components",
+              "@assets": "./assets",
+              "@screens": "./screens",
+              "@styles": "./styles",
+              "@icons": "./components/icons",
+            },
+          }],
+        ],
+      }
+    }
   };
 };
