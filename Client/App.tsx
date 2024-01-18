46e07d4be331e1b83d4627c7937ff46146460ea9
bvsantos <bvsantosdev@gmail.com>
2024-01-18 18:03:37 +0000

    merge

--- Client/App.tsx
+++ Client/App.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import AccountTypeSelection from '@screens/AccountTypeSelection'
 
 export default function App() {
+  const [test, setTest] = useState("");
   return (
     <AccountTypeSelection/>
   );
