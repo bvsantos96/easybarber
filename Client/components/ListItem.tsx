8bb5125284ce8d0e0b0b778183c9e58ea26048a8
Filipe Santos <filipesantosdev@gmail.com>
2024-01-22 02:16:51 +0000

    feat: List of items: components and styles created

---
+++ Client/components/ListItem.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { styles } from "../styles/List";
+import { BarberInfo } from "../utils/ApiRequest";
+
+export default function ListItem({ barber }: { barber: BarberInfo }}) {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
--- Client/screens/Home.tsx
+++ Client/screens/Home.tsx
@@ -1,12 +1,33 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import TopBar from '../components/TopBar';
 
 import { styles } from '../styles/Main';
+import { useEffect, useState } from 'react';
+import { BarberInfo, getBarbersNearMe } from '../utils/ApiRequest';
+import ListItem from '../components/ListItem';
 
 export default function Home() {
+    const [barberList, setBarberList] = useState<BarberInfo[]>([]);
+
+    useEffect(()=>{
+        const fetchBarbers = async () => {
+            const barbers: BarberInfo[] = await getBarbersNearMe();
+            setBarberList(barbers);
+        }
+
+        fetchBarbers();
+    },[]);
+
     return (
         <View style={styles.container}>
             <TopBar />
+            <ScrollView>
+                {barberList && barberList.map((barber: BarberInfo) => {
+                    return (
+                        <ListItem key={barber.id} barber={barber} />
+                    );
+                })}
+            </ScrollView>
         </View>
     );
 }
