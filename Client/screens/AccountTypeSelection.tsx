2547a97df15ee908a8f830b330a5d4330bb4b683
bvsantos <bvsantosdev@gmail.com>
2024-01-22 17:26:00 +0000

    feat: Mazzard font and initial account type slection mockup

--- Client/screens/AccountTypeSelection.tsx
+++ Client/screens/AccountTypeSelection.tsx
@@ -1,7 +1,11 @@
 import React, { useRef, FC } from 'react';
-import { View, TextInput, TouchableOpacity, StyleProp, ViewStyle, TextInputProps } from 'react-native';
-import Button from '@components/Button';
-import {styles} from '@styles/main';
+import {View, Image} from 'react-native';
+import Button from '../components/Button';
+import {styles as screenStyles} from '../styles/Screens';
+import {styles as mainStyles} from '../styles/Main';
+import { textBlackColor } from '../styles/Main';
+import {thinBorderBlack, whitheButtonColor} from '../styles/Button'
+import Divider from '../components/Divider';
 
 interface InputProps {
   icon: () => React.ReactNode;
@@ -12,9 +16,15 @@ interface InputProps {
 
 const AccountTypeSelection: FC<InputProps> = () => {
   return (
-    <View style={styles.container}>
-      <Button title="I'm a user" onPress={()=>alert("test")}/>
-      <Button title="I'm a barber" onPress={()=>alert("test")}/>
+    <View style={mainStyles.containerSpaceBetween}>
+      <View/>
+      <Image style={screenStyles.centeredLogo} source={require("@assets/images/logo.png")}/>
+      <View style={[mainStyles.w100, mainStyles.paddingBottom10]} >
+        <Button title="I'm a User" onPress={()=>alert("test")}/>
+        <Divider/>
+        <Button title="I'm a Barber" backgroundColor={whitheButtonColor} buttonTextColor={textBlackColor} borderColor={thinBorderBlack} onPress={()=>alert("test")}/>
+        <Divider/>
+      </View>
     </View>
   );
 };
--- Client/styles/main.tsx
+++ Client/styles/main.tsx
@@ -1,21 +1,29 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, DimensionValue } from 'react-native';
 
 const window = Dimensions.get("window");
 export const inputHeight = window.height * 0.07;
 export const inputWidth = '85%';
 export const mainColor = "#DF2238";
 export const secondColor = "rgba(109, 4, 4, 0.10)";
+export const textBlackColor = "#263238";
 export const inputTextColor = "black";
 export const backgroundColor = "white";
 export const width = window.width;
 export const height = window.height;
 
 export const styles = StyleSheet.create({
-    container: {
+    containerSpaceBetween: {
         flex: 1,
         justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: backgroundColor,
+    },
+    containerCenter: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: backgroundColor,
+        height: '100vh' as DimensionValue
     },
     hMargin5: {
         marginHorizontal: 5
@@ -27,4 +35,11 @@ export const styles = StyleSheet.create({
         minWidth: '100%',
         alignItems: 'center',
     },
+    fixBottom:{
+        position: 'absolute',
+        bottom: 0
+    },
+    paddingBottom10:{
+        paddingBottom: '50%'
+    }
 });
