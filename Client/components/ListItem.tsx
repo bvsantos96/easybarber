4e9b0334dcd35de168be7c94889a6f17e9801387
Filipe Santos <filipesantosdev@gmail.com>
2024-01-24 20:06:05 +0000

    feat: barberListItem Done

--- Client/components/ListItem.tsx
+++ Client/components/ListItem.tsx
@@ -1,6 +1,6 @@
 import { View, Image, Text } from "react-native";
 import { styles } from "../styles/List";
-import {styles as mainStyles} from "../styles/Main";
+import { styles as mainStyles } from "../styles/Main";
 import { BarberInfo } from "../utils/ApiRequest";
 import Pressable from "../components/Pressable";
 
@@ -8,17 +8,15 @@ export default function ListItem({ barber }: { barber: BarberInfo }) {
     const texts = require("../langs/en.json");
     return (
         <Pressable onPress={()=>{alert(`Open barber ${barber.name}`);}} style={[styles.container]}>
-            <View style={{paddingRight: 5}}>
-                <View style={[styles.ratingContainer, mainStyles.zIndex10, mainStyles.alignCenter, mainStyles.justifyCenter]}>
+            <Image source={{ uri: barber.photo }} style={styles.imageStyle} />
+            <View  style={styles.ratingContainer} >
+                <View style={[styles.ratingIconContainer, mainStyles.alignCenter, mainStyles.justifyCenter]}>
                     <Image style={styles.ratingIcon} source={require('@assets/icons/star.png')} />
-                    <View style={[mainStyles.row, mainStyles.alignCenter, mainStyles.justifyCenter]}>
-                        <Text style={styles.ratingText}>{barber.rating.toFixed(1)}</Text>
-                        <Text style={styles.numberOfVotes}>({barber.nVotes})</Text>
-                    </View>
                 </View>
-                <Image source={{ uri: barber.photo }} style={styles.imageStyle} />
+                <Text style={styles.ratingText}>{barber.rating.toFixed(1)}</Text>
+                <Text style={styles.numberOfVotes}>({barber.nVotes})</Text>
             </View>
-            <View style={styles.textContainer}>
+            <View style={styles.textContainer} >
                 <Text style={styles.title}>{barber.name}</Text>
                 <View style={styles.locationContainer}>
                     <Image style={styles.locationIcon} source={require('@assets/icons/location.png')} />
