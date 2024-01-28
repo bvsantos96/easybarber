8fb840381fb811bc414663a0d0c77026c7b40237
Filipe Santos <filipesantosdev@gmail.com>
2024-01-28 20:01:26 +0000

    feat: Main layout changes done on filter

--- Client/screens/Filter.tsx
+++ Client/screens/Filter.tsx
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { BottomSheetBackdrop, BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 import { styles } from '../styles/Filter';
+import Button from '../components/Button';
 
 interface FilterProps { }
 
@@ -11,8 +12,10 @@ export interface FilterRef {
 }
 
 const Filter = forwardRef<FilterRef, FilterProps>(({ }, ref) => {
+    const [starsSelected, setStarsSelected] = React.useState(5);
+    const texts = require("@lang/en.json");
     const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-    const snapPoints = useMemo(() => ["60%"], []);
+    const snapPoints = useMemo(() => ["70%"], []);
     const handlePresentModalPress = useCallback(() => {
         bottomSheetModalRef.current?.present();
     }, []);
@@ -50,10 +53,21 @@ const Filter = forwardRef<FilterRef, FilterProps>(({ }, ref) => {
                         }} {...backdropProps} />
                 )}
                 onChange={handleSheetChanges} >
-                <View>
-                    <Text>Filter</Text>
+                <View style={styles.topBarContainer}>
+                    <Text style={styles.title}>{texts.findBarber}</Text>
+                    <Text style={styles.clear}>{texts.clear}</Text>
                 </View>
-            </BottomSheetModal>
+                <View style={styles.input} />
+                <View style={styles.ratingTitleContainer}>
+                    <Text style={styles.ratingTitle}>{texts.rating}</Text>
+                    <Text style={styles.ratingStars}>{starsSelected} {texts.stars}</Text>
+                </View>
+                <Text style={styles.availableTimeTitle}>{texts.availableTime}</Text>
+                <View style={styles.timeSelectionContainer} />
+                <View style={styles.applyContainer}>
+                    <Button title={texts.apply} />
+                </View>
+           </BottomSheetModal>
         </BottomSheetModalProvider>
     );
 });
