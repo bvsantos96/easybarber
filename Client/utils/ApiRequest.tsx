dc4d1a616d3f0239883eb6ea0a906eb203aa5c16
Filipe Santos <filipesantosdev@gmail.com>
2024-01-29 02:36:49 +0000

    feat: GetTimes updated

--- Client/utils/ApiRequest.tsx
+++ Client/utils/ApiRequest.tsx
@@ -51,3 +51,29 @@ import { PickerItem } from '../components/Picker';
 export const getCategories = async (): Promise<PickerItem[]> => {
     return comboboxes.categories;
 }
+
+export const getTimes = async ({from, to}:{from? :string, to?: string}) : Promise<PickerItem[]> => { 
+    from = from || "08:00";
+    to = to || "20:00";
+    let startHour = parseInt(from.split(":")[0]);
+    let startMinute = parseInt(from.split(":")[1]);
+    let endHour = parseInt(to.split(":")[0]);
+    let endMinute = parseInt(to.split(":")[1]);
+    let times: PickerItem[] = [];
+    for(let i = startHour; i < 20; i++){
+        for(let j = startHour == i ? startMinute : 0; j < 60; j+=30){
+            if(endHour <= i && endMinute <=j)
+                return times;
+            let time = `${i}:${j == 0 ? "00" : j}`;
+            times.push({
+                label: time,
+                value: time
+            });
+            if(startHour == i && startMinute == j){
+                j = startMinute + 30 > 60? 30 : 0;
+            }
+        }
+    }
+
+    return times;
+}
