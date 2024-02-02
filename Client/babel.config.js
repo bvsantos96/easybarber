5927457e2f6dd1ca236f947311b908087b52b223
Filipe Santos <filipesantosdev@gmail.com>
2024-02-02 23:10:53 +0000

    feat: svg coloring

--- Client/babel.config.js
+++ Client/babel.config.js
@@ -1,26 +1,39 @@
-module.exports = function(api) {
+module.exports = ((api) => {
     api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        env: {
-            development: {
-                plugins: [
-                    [
-                        'module-resolver',
-                        {
-                            alias: {
-                                "@components": "./components",
-                                "@assets": "./assets",
-                                "@screens": "./screens",
-                                "@styles": "./styles",
-                                "@icons": "./components/icons",
-                                "@lang": "./langs",
-                            },
-                        }
-                    ],
-                    'react-native-reanimated/plugin',
-                ],
-            }
-        }
+
+    const config = getDefaultConfig(__dirname);
+
+    const { transformer, resolver } = config;
+
+    config.transformer = {
+        ...transformer,
+        babelTransformerPath: require.resolve("react-native-svg-transformer")
     };
-};
+    config.resolver = {
+        ...resolver,
+        assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...resolver.sourceExts, "svg"]
+    };
+    config.presets = ['babel-preset-expo'];
+    config.env =  { 
+        development: {
+            plugins: [
+                [
+                    'module-resolver',
+                    {
+                        alias: {
+                            "@components": "./components",
+                            "@assets": "./assets",
+                            "@screens": "./screens",
+                            "@styles": "./styles",
+                            "@icons": "./components/icons",
+                            "@lang": "./langs",
+                        },
+                    }
+                ],
+                'react-native-reanimated/plugin',
+            ],
+        }
+    }
+    return config;
+});
