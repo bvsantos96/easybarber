27bc9a99c3d0557b745e5e4c1d326a6bf3669baf
Filipe Santos <filipesantosdev@gmail.com>
2024-02-12 00:58:11 +0000

    feat: Finished style adjustments

--- Client/components/ListItemBarbershop.tsx
+++ Client/components/ListItemBarbershop.tsx
@@ -1,6 +1,6 @@
 import { View, Image, Text } from "react-native";
-import { styles } from "../styles/List";
-import { absoluteWidth, styles as mainStyles } from "../styles/Main";
+import { getStyles } from "../styles/List";
+import { getStyles as mainGetStyles } from "../styles/Main"
 import { BarberInfo } from "../utils/ApiRequest";
 import Pressable from "../components/Pressable";
 
@@ -8,11 +8,13 @@ import StarIcon from '@assets/icons/star.svg';
 import LocationIcon from '@assets/icons/location.svg';
 
 export default function ListItem({ barber }: { barber: BarberInfo }) {
+    const styles = getStyles();
+    const mainStyles = mainGetStyles();
     const texts = require("../langs/en.json");
     return (
-        <Pressable onPress={()=>{alert(`Open barber ${barber.name}`);}} style={[styles.container]}>
+        <Pressable onPress={() => { alert(`Open barber ${barber.name}`); }} style={[styles.container]}>
             <Image source={{ uri: barber.photo }} style={styles.imageStyle} />
-            <View  style={[styles.ratingContainer, mainStyles.shadow]} >
+            <View style={[styles.ratingContainer, mainStyles.shadow]} >
                 <View style={[styles.ratingIconContainer, mainStyles.alignCenter, mainStyles.justifyCenter, mainStyles.shadow]}>
                     <StarIcon style={styles.ratingIcon} width="75%" height="75%" />
                 </View>
@@ -22,7 +24,7 @@ export default function ListItem({ barber }: { barber: BarberInfo }) {
             <View style={styles.textContainer} >
                 <Text style={styles.title}>{barber.name}</Text>
                 <View style={styles.locationContainer}>
-                    <LocationIcon width={13 * absoluteWidth} height={13 * absoluteWidth} style={styles.locationIcon} />
+                    <LocationIcon width={styles.locationIcon.width} height={styles.locationIcon.height} style={styles.locationIcon} />
                     <Text style={styles.locationText}>{barber.distance.toFixed(1)}{texts.kmAway}</Text>
                 </View>
                 <Text style={styles.description}>{barber.description}</Text>
--- Client/screens/Tabs.tsx
+++ Client/screens/Tabs.tsx
@@ -1,54 +1,55 @@
 import Home from './Home';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { absoluteWidth, inputTextColor, mainColor } from '../styles/Main';
 import Appointments from './Appointments';
 import HomeIcon from "@assets/icons/home.svg";
 import AppointmentsIcon from "@assets/icons/appointments.svg";
 import AlertsIcon from "@assets/icons/alerts.svg";
 import ChatsIcon from "@assets/icons/chats.svg";
+import { useTheme } from '../styles/ThemeContext';
 
 export default function Tabs() {
     const Tab = createBottomTabNavigator();
     const texts = require('../langs/en.json');
+    const theme = useTheme();
 
     return (
         <Tab.Navigator screenOptions={{
-            tabBarActiveTintColor: mainColor,
-            tabBarInactiveTintColor: inputTextColor,
+            tabBarActiveTintColor: theme.colors.mainColor,
+            tabBarInactiveTintColor: theme.colors.text.main,
         }}>
             <Tab.Screen name={texts.tabs.home} component={Home}
                 options={{
                     headerShown: false,
                     tabBarIcon: ({ focused }) => !focused ? (
-                        <HomeIcon width={20 * absoluteWidth} height={20 * absoluteWidth} />
+                        <HomeIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} />
                     ) : (
-                        <HomeIcon width={20 * absoluteWidth} height={20 * absoluteWidth} fill={mainColor} />
+                        <HomeIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} fill={theme.colors.mainColor} />
                     )
                 }} />
             <Tab.Screen name={texts.tabs.appointments} component={Appointments}
                 options={{
                     tabBarIcon: ({ focused }) => !focused ? (
-                        <AppointmentsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} />
+                        <AppointmentsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} />
                     ) : (
-                        <AppointmentsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} fill={mainColor} />
+                        <AppointmentsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} fill={theme.colors.mainColor} />
                     )
                 }} />
             <Tab.Screen name={texts.tabs.alerts} component={Home}
                 options={{
                     headerShown: false,
                     tabBarIcon: ({ focused }) => !focused ? (
-                        <AlertsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} />
+                        <AlertsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} />
                     ) : (
-                        <AlertsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} fill={mainColor} />
+                        <AlertsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} fill={theme.colors.mainColor} />
                     )
                 }} />
             <Tab.Screen name={texts.tabs.chats} component={Home}
                 options={{
                     headerShown: false,
                     tabBarIcon: ({ focused }) => !focused ? (
-                        <ChatsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} />
+                        <ChatsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} />
                     ) : (
-                        <ChatsIcon width={20 * absoluteWidth} height={20 * absoluteWidth} fill={mainColor} />
+                        <ChatsIcon width={20 * theme.dimensions.absoluteWidth} height={20 * theme.dimensions.absoluteWidth} fill={theme.colors.mainColor} />
                     )
                 }} />
         </Tab.Navigator>
