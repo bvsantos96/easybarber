259cb6a125752e7305d65595841d3e8fe4190000
Filipe Santos <filipesantosdev@gmail.com>
2024-02-19 01:12:32 +0000

    feat: Removed console.log

--- Client/styles/ThemeContext.tsx
+++ Client/styles/ThemeContext.tsx
@@ -95,7 +95,6 @@ export const useTheme = (): Theme => {
     if (!context) {
         throw new Error('useTheme must be used within a ThemeProvider');
     }
-    console.log(defaultTheme);
     return context;
 };
 
