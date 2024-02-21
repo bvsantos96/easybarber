5841817c283056f90147fb784533defe68d8a54b
bvsantos <bvsantosdev@gmail.com>
2024-02-21 20:36:06 +0000

    fix: Missing modelmapper bean

--- Server/src/main/java/com/teamsantos/easybarber/EasybarberApplication.java
+++ Server/src/main/java/com/teamsantos/easybarber/EasybarberApplication.java
@@ -1,11 +1,16 @@
 package com.teamsantos.easybarber;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EasybarberApplication {
-
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EasybarberApplication.class, args);
 	}
