8c5719b19e0a79918fe67e6122cc72a2aede3a37
bvsantos <bvsantosdev@gmail.com>
2024-02-21 20:32:54 +0000

    feat: User service implmentation

--- Server/src/main/java/com/teamsantos/easybarber/controllers/UserController.java
+++ Server/src/main/java/com/teamsantos/easybarber/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.teamsantos.easybarber.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teamsantos.easybarber.DTO.UserCreateDTO;
+import com.teamsantos.easybarber.DTO.UserDTO;
+import com.teamsantos.easybarber.DTO.UsersDTO;
 import com.teamsantos.easybarber.services.UserService;
 
 @RestController
@@ -20,27 +24,46 @@ public class UserController {
     private UserService userService;
 
     @GetMapping()
-    public String getAllUsers() {
-        userService.getAllUsers();
-        return "Hello, World!";
+    public ResponseEntity<UsersDTO> getAllUsers() {
+        UsersDTO response = new UsersDTO();
+        try {
+            response.setUsers(userService.getAllUsers());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.setResponseMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 
     @PostMapping()
-    public String createUser(@RequestBody UserCreateDTO userDTO) {
-        userService.createUser();
-        return "Hello, World!";
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserCreateDTO userDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDTO));
+        } catch (Exception e) {
+            UserDTO response = new UserDTO();
+            response.setResponseMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 
     @PutMapping()
-    public String updateUser(@RequestBody UserCreateDTO userDTO) {
-        userService.updateUser();
-        return "Hello, World!";
+    public ResponseEntity<String> updateUser(@RequestBody UserCreateDTO userDTO) {
+        try {
+            userService.updateUser(userDTO);
+            return ResponseEntity.ok("User updated successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update user: " + e.getMessage());
+        }
     }
 
     @DeleteMapping()
-    public String deleteUser(@RequestBody UserCreateDTO userDTO) {
-        userService.deleteUser();
-        return "Hello, World!";
+    public ResponseEntity<String> deleteUser(@RequestBody UserCreateDTO userDTO) {
+        try {
+            userService.deleteUser(userDTO);
+            return ResponseEntity.ok("User deleted successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete user: " + e.getMessage());
+        }
     }
 
 }
