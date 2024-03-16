a9458641fa42a449fb9de78181c7cdc8db817d50
Filipe Santos <filipesantosdev@gmail.com>
2024-03-16 22:41:30 +0000

    fix: Error in directory capitalization

---
+++ Server/src/main/java/com/teamsantos/easybarber/exceptions/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.teamsantos.easybarber.exceptions;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserNotFoundException extends UsernameNotFoundException {
+
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
