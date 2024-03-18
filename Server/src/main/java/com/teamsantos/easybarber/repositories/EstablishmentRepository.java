79bfcce870deefeea9890fe1358acbeee9706d84
bvsantos <bvsantosdev@gmail.com>
2024-03-18 20:59:46 +0000

    fix: removing possible nulls from User Repo

--- Server/src/main/java/com/teamsantos/easybarber/repositories/EstablishmentRepository.java
+++ Server/src/main/java/com/teamsantos/easybarber/repositories/EstablishmentRepository.java
@@ -2,9 +2,11 @@ package com.teamsantos.easybarber.repositories;
 
 import com.teamsantos.easybarber.DTO.BaseEstablishmentDTO;
 import com.teamsantos.easybarber.entities.Establishment;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +14,11 @@ import java.util.Optional;
 
 @Repository
 public interface EstablishmentRepository extends JpaRepository<Establishment, Long> {
-	Optional<Establishment> findById(Long id);
-	Optional<Establishment> findByName(String name);
+	@NonNull
+	Optional<Establishment> findById(@NonNull Long id);
+
+	Optional<Establishment> findByName(@NonNull String name);
+	
 	@Query("SELECT new com.teamsantos.easybarber.DTO.BaseEstablishmentDTO(e.id, e.name, e.description) FROM Establishment e")
 	List<BaseEstablishmentDTO> findAllBase(Pageable pageable);
 }
