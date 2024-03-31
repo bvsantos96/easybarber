58b59c6d4415cba66fd87f2840996d31233bf73a
Filipe Santos <filipesantosdev@gmail.com>
2024-03-31 18:32:19 +0100

    feat: Added AuthTests

---
+++ Server/src/test/java/com/teamsantos/easybarber/testData/UsersData.java
@@ -0,0 +1,22 @@
+package com.teamsantos.easybarber.testData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class UsersData {
+    public static final List<JSONObject> users;
+
+    static {
+        users = new ArrayList<>();
+        try {
+            users.add(new JSONObject(
+                    "{\"countryMobile\":\"+351\",\"mobile\":\"927030780\",\"password\":\"Test123*\",\"name\":\"Bruno Santos\"}"));
+            users.add(new JSONObject(
+                    "{\"countryMobile\":\"+351\",\"mobile\":\"962844407\",\"password\":\"Test123*\",\"name\":\"Filipe Santos\"}"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
