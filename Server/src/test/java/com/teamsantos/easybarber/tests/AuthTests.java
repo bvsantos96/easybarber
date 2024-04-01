4127c5e14026657b4a649107bfd0370cf0265b0e
Filipe Santos <filipesantosdev@gmail.com>
2024-04-01 21:28:32 +0100

    feat: dev

--- Server/src/test/java/com/teamsantos/easybarber/tests/AuthTests.java
+++ Server/src/test/java/com/teamsantos/easybarber/tests/AuthTests.java
@@ -1,77 +1,58 @@
 package com.teamsantos.easybarber.tests;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.teamsantos.easybarber.testData.UsersData;
-import com.teamsantos.easybarber.utils.AnyOfStatusMatcher;
+import com.teamsantos.easybarber.utils.CreateTest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AuthTests {
     private final MockMvc mockMvc;
+    public static boolean created = false;
 
     @Autowired
     public AuthTests(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
     }
 
-    private void _registerUser(String user) throws Exception {
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
-                .post("/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(user));
-        result
-                .andExpect(MockMvcResultMatchers.status()
-                        .isCreated());
-    }
-
-    public void registerUser(String user) throws Exception {
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
-                .post("/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(user));
-        result
-                .andExpect(MockMvcResultMatchers.status()
-                        .is(AnyOfStatusMatcher.createdOrFound()));
+    public String loginUser() throws Exception {
+        if(!created)
+            test();
+        return loginUser(UsersData.users.get(0).toString());
     }
 
     public String loginUser(String user) throws Exception {
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
-                .post("/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(user));
+        ResultActions result = CreateTest.post(mockMvc, "/login", user);
         result
                 .andExpect(MockMvcResultMatchers.status().isOk());
         return result.andReturn().getResponse().getContentAsString();
     }
 
+    private void create(String path, String item) throws Exception {
+        if (!created)
+            CreateTest.create(mockMvc, path, item);
+        else
+            CreateTest.createOrFound(mockMvc, path, item);
+    }
+
     @Test
     public void test() {
         try {
-            _registerUser(UsersData.users.get(0).toString());
-            _registerUser(UsersData.users.get(1).toString());
+            create("/register", UsersData.users.get(0).toString());
+            create("/register", UsersData.users.get(1).toString());
             loginUser(UsersData.users.get(1).toString());
+            if (!created)
+                created = true;
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public String testAuth() {
-        try {
-            registerUser(UsersData.users.get(0).toString());
-            registerUser(UsersData.users.get(1).toString());
-            return loginUser(UsersData.users.get(1).toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
         }
     }
 }
--- Server/src/test/java/com/teamsantos/easybarber/tests/ServiceTests.java
+++ Server/src/test/java/com/teamsantos/easybarber/tests/ServiceTests.java
@@ -1,17 +1,44 @@
 package com.teamsantos.easybarber.tests;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.teamsantos.easybarber.testData.ServiceData;
+import com.teamsantos.easybarber.utils.CreateTest;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ServiceTests {
     private final MockMvc mockMvc;
+    public static boolean created = false;
 
     @Autowired
     public ServiceTests(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
+    }
+
+    private void create(String path, String jwt, String item) throws Exception {
+        if (!created)
+            CreateTest.create(mockMvc, path, jwt, item);
+        else
+            CreateTest.createOrFound(mockMvc, path, jwt, item);
+    }
+
+    @Test
+    public void test() {
+        try {
+            String jwt = new EmployeeTests(mockMvc).loginUser();
+            create("/service", jwt, ServiceData.serviceTypes.get(0).toString());
+            create("/service", jwt, ServiceData.serviceTypes.get(1).toString());
+            create("/service", jwt, ServiceData.serviceTypes.get(2).toString());
+            if (!created)
+                created = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
