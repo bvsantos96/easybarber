da5e95cec720e77044c86b44e6c6625513594789
bvsantos <bvsantosdev@gmail.com>
2024-04-02 20:20:07 +0100

    feat: repos and service files

---
+++ Server/src/main/java/com/teamsantos/easybarber/controllers/EstablishmentStaffController.java
@@ -0,0 +1,46 @@
+package com.teamsantos.easybarber.controllers;
+
+import com.teamsantos.easybarber.DTO.BasePageDTO;
+import com.teamsantos.easybarber.DTO.UserCreateDTO;
+import com.teamsantos.easybarber.DTO.UserDTO;
+import com.teamsantos.easybarber.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.Principal;
+
+@Controller
+public class EstablishmentStaffController {
+    private final EstablishmentStafFS userService;
+
+    @Autowired
+    public EstablishmentStaffController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<BasePageDTO<UserDTO>> getAllUsers(Pageable pageable) {
+        try {
+            return ResponseEntity.ok(new BasePageDTO<>(userService.getAllUsers(pageable)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new BasePageDTO<>(e.getMessage()));
+        }
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<String> updateUser(@RequestBody UserCreateDTO userDTO, Principal principal) {
+        try {
+            userService.updateUser(userDTO, principal);
+            return ResponseEntity.ok("User updated successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to update user: " + e.getMessage());
+        }
+    }
+}
---
+++ Server/src/main/java/com/teamsantos/easybarber/repositories/EstablishmentServiceRepository.java
@@ -0,0 +1,10 @@
+package com.teamsantos.easybarber.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teamsantos.easybarber.entities.EstablishmentService;
+
+@Repository
+public interface EstablishmentServiceRepository extends JpaRepository<EstablishmentService, Long> {
+}
