8ecea06150540eea49e0e4d0bd3782764e9ec341
Filipe <filipesantosdev@gmail.com>
2024-04-07 03:47:49 +0100

    feat: Created tests to check user list method

---
+++ Server/src/main/java/com/teamsantos/easybarber/utils/JSONToDTO.java
@@ -0,0 +1,71 @@
+package com.teamsantos.easybarber.utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONToDTO {
+
+    public static String getString(Object obj, String fieldName) {
+        try {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return (String) field.get(obj);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
+        }
+    }
+
+    public static Long getLong(Object obj, String fieldName) {
+        try {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return (Long) field.get(obj);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
+        }
+    }
+
+    public static <T> List<T> fromBaseDTO(JSONObject jsonObject, Class<T> clazz) {
+        try {
+            ArrayList<T> list = new ArrayList<T>();
+            jsonObject = jsonObject.getJSONObject("items");
+            JSONArray arr = jsonObject.getJSONArray("content");
+            for (int i = 0; i < arr.length(); i++) {
+                list.add(toDTO(arr.getJSONObject(i), clazz));
+            }
+            return list;
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException
+                | NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static <T> T toDTO(JSONObject jsonObject, Class<T> clazz) throws NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Constructor<T> constructor = clazz.getConstructor();
+        T instance = constructor.newInstance();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            String fieldName = field.getName();
+            if (jsonObject.has(fieldName)) {
+                field.setAccessible(true);
+                Class<?> fieldType = field.getType();
+                if (fieldType == int.class) {
+                    field.setInt(instance, jsonObject.getInt(fieldName));
+                } else if (fieldType == String.class) {
+                    field.set(instance, jsonObject.getString(fieldName));
+                }
+                // Add more type mappings as needed
+            }
+        }
+
+        return instance;
+    }
+}
