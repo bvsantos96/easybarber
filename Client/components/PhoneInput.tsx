3efa4c24ada8b06d058fb1ad3abff600b337e5db
Filipe <filipesantosdev@gmail.com>
2024-04-17 14:14:53 +0100

    feat: Added countryCode selection for login

---
+++ Client/components/PhoneInput.tsx
@@ -0,0 +1,39 @@
+import { SetStateAction, Dispatch } from 'react';
+import Input from '../components/Input';
+import CountryPicker, { Country } from 'react-native-country-picker-modal';
+
+interface NationSelectionProps {
+    setNation: Dispatch<SetStateAction<Country | null | undefined>>;
+    nation: Country | null | undefined; 
+}
+
+interface PhoneInputProps extends NationSelectionProps {
+    setPhone: Dispatch<SetStateAction<string>>;
+}
+
+export default function PhoneInput({ setPhone, setNation, nation}: PhoneInputProps) {
+    const texts = require("@lang/en.json");
+
+    return (
+        <Input
+            icon={<NationPicker {...{ setNation, nation }} />}
+            placeholder={texts.phoneNumber}
+            type="tel"
+            onInputChange={setPhone}
+        />
+    )
+}
+
+export function NationPicker({ nation, setNation }: NationSelectionProps) {
+    return (
+        <CountryPicker
+            countryCode={nation ? nation.cca2 : "PT"}
+            withFilter
+            withFlag
+            withCallingCode
+            withEmoji
+            onSelect={(country: Country) => { country && setNation(country) }}
+            visible
+        />
+    );
+}
