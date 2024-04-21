88da36f833fb8b1be687f894201d1970be2feae4
Filipe Santos <filipesantosdev@gmail.com>
2024-04-21 11:30:26 +0100

    feat: dev

--- Server/src/main/java/com/teamsantos/easybarber/entities/base/EntityWithImages.java
+++ Server/src/main/java/com/teamsantos/easybarber/entities/base/EntityWithImages.java
@@ -1,29 +1,50 @@
 package com.teamsantos.easybarber.entities.base;
 
-import java.util.HashSet;
 import java.util.Set;
 
-import com.teamsantos.easybarber.entities.Image;
+import org.modelmapper.TypeToken;
+
+import com.teamsantos.easybarber.DTO.ImageDTO;
+import com.teamsantos.easybarber.utils.Utils;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
-public abstract class EntityWithImages {
+public abstract class EntityWithImages<T> {
     @OneToMany(mappedBy = "entity", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Image> images = new HashSet<>();
+    private Set<Image<T>> images;
 
-    public Set<Image> getImages() {
+    public Set<Image<T>> getImages() {
         setImages(images);
         return images;
     }
 
-    public void setImages(Set<Image> images) {
-        for(final Image image : images)
-            image.setEntity(this);
+    public void addImage(ImageDTO image) {
+        getImages().add(Utils.getModelMapper().map(image, new TypeToken<Image<T>>() {
+        }.getType()));
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void addImage(Image image) {
+        getImages().add(Utils.getModelMapper().map(image, new TypeToken<Image<T>>() {
+        }.getType()));
+    }
+
+    public void removeImage(Image<T> image) {
+        getImages().remove(image);
+    }
+
+    public void setImages(Set<Image<T>> images) {
+        for (final Image<T> image : images)
+            image.setEntity(getEntity());
         this.images = images;
     }
 
-    public abstract String getEntityType();
+    public abstract T getEntity();
 }
