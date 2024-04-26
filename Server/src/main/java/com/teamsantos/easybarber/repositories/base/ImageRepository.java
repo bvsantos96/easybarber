89431a93853a801afb4ac669e68b3cfdf3a11296
Filipe Santos <filipesantosdev@gmail.com>
2024-04-26 01:24:19 +0100

    feat: dev

--- Server/src/main/java/com/teamsantos/easybarber/repositories/base/ImageRepository.java
+++ Server/src/main/java/com/teamsantos/easybarber/repositories/base/ImageRepository.java
@@ -2,6 +2,7 @@ package com.teamsantos.easybarber.repositories.base;
 
 import com.teamsantos.easybarber.entities.base.EntityWithImages;
 import com.teamsantos.easybarber.entities.base.Image;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,8 @@ import org.springframework.data.repository.query.Param;
 @NoRepositoryBean
 public interface ImageRepository<T extends EntityWithImages<T, E>, E extends Image<T, E>>
         extends JpaRepository<E, Long> {
-    Long getIdByEntityAndData(T entity, String data);
+    @Query("SELECT i.id FROM #{#entityName} i WHERE i.entity.id = :entityId AND i.data = :data")
+    Long getIdByEntityIdAndData(Long entityId, String data);
 
     @Modifying
     @Query("DELETE FROM #{#entityName} i WHERE i.entity.id = :id")
