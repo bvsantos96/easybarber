729f59c8b3607af6e7f985d17dde602905d2a999
Filipe Santos <filipesantosdev@gmail.com>
2024-05-19 19:17:57 +0100

    feat: Added version to every api response and a new filter to establishment search

---
+++ Server/src/main/java/com/teamsantos/easybarber/DTO/filters/EstablishmentFilter.java
@@ -0,0 +1,27 @@
+package com.teamsantos.easybarber.DTO.filters;
+
+import java.time.LocalTime;
+
+import org.locationtech.jts.geom.Point;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstablishmentFilter {
+    private String name;
+    private String description;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Point location;
+    private Double rating;
+    private Long serviceType;
+    private LocalTime from;
+    private LocalTime to;
+}
