f780f413ba67af56e2c6f64fefb4ba2d561d09af
Filipe Santos <filipesantosdev@gmail.com>
2024-06-17 18:40:47 +0100

    feat: Update to expo 51

--- Client/components/PhoneInput.tsx
+++ Client/components/PhoneInput.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { SetStateAction, Dispatch } from 'react';
 import Input from '../components/Input';
 import CountryPicker, { Country } from 'react-native-country-picker-modal';
