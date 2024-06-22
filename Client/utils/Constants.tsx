f73c4294eb0d18e1bbc1ed4a6c0fa1f63e98d2ad
bvsantos96 <bvsantosdev@gmail.com>
2024-06-22 12:16:46 +0100

    feat: defaultCountry now is set to device settings

--- Client/utils/Constants.tsx
+++ Client/utils/Constants.tsx
@@ -1,17 +1,26 @@
 import { getAllCountries, FlagType, Country } from 'react-native-country-picker-modal';
+import {getLocales} from 'expo-localization';
 
 let defaultCountryType = process.env.EXPO_PUBLIC_DEFAULT_COUNTRY;
 
 export const getDefaultCountryAsync = async (): Promise<Country | undefined> => {
-    const defaultCountry = await getAllCountries(FlagType.FLAT);
+    try {
+        const deviceCountryCode = getLocales()[0].regionCode;
+        if (!deviceCountryCode) {
+            throw new Error("Device country code is undefined.");
+        }
 
-    const filteredCountry = defaultCountry?.filter(
-        (c) => c.cca2 === defaultCountryType || c.cca2 === "PT"
-    )[0];
+        const allCountries = await getAllCountries(FlagType.FLAT);
+        
+        const filteredCountry = allCountries.find((c) => c.cca2 === deviceCountryCode);
 
-    return filteredCountry;
+        return filteredCountry;
+    } catch (error) {
+        console.error("Error fetching default country:", error);
+        return undefined;
+    }
 };
 
 export const getDefaultCountryString = (): string => {
     return defaultCountryType || "PT";
-}
+};
