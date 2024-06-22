c3a1b23c1b28efadac4d10049bfad85174a22faf
Filipe Santos <filipesantosdev@gmail.com>
2024-06-22 16:33:41 +0100

    feat: Modal to change location now is filled with the location list

--- Client/components/ChangeLocation.tsx
+++ Client/components/ChangeLocation.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import { retrieveLocations } from '../storage/ApiLongTermStorage';
+import { ILocation } from '../declarations';
 
 export default function ChangeLocation() {
+    const [locations, setLocations] = useState<ILocation[]>();
+    useEffect(() => {
+        retrieveLocations().then((_locations) => {
+            setLocations(_locations);
+        });
+    }, []);
     return (
         <View>
             <TouchableOpacity onPress={async () => { console.log(await retrieveLocations()) }}>
-                <Text>List locations</Text>
+                {locations?.map((location, idx) => {
+                    return (
+                        <View key={idx}>
+                            <Text>{location.address}</Text>
+                        </View>
+                    );
+                })}
             </TouchableOpacity>
         </View>
     );
