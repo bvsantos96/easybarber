0fc659a7864e8d190404676447f67e13c08ce9d9
Filipe Santos <filipesantosdev@gmail.com>
2024-06-22 15:54:21 +0100

    feat: Added localstorage keys to constants file

--- Client/storage/ApiLongTermStorage.ts
+++ Client/storage/ApiLongTermStorage.ts
@@ -2,13 +2,14 @@ import { ICategory, ILocation } from "../declarations";
 import { getCategories, getLocations, setNewLocation } from "../utils/ApiRequest";
 import { clearAll, getArray, getArrayOrEmpty, store } from "./StorageUtils";
 import { getLocation } from "../utils/Location";
+import { CATEGORY_STORAGE_KEY, LOCATIONS_STORAGE_KEY } from "../utils/Constants";
 
 // TODO: This function needs to validate if it gets a valid responses otherwise it needs to be retried
 // if after that it fails we need to show an error message to the user, this is a critical function
 export const loadLongTermItems = async (): Promise<Boolean> => {
     try {
         await clearAll();
-        await store("categories", JSON.stringify(await getCategories()));
+        await store(CATEGORY_STORAGE_KEY, JSON.stringify(await getCategories()));
         return true;
     } catch (e) {
         return false;
@@ -16,20 +17,20 @@ export const loadLongTermItems = async (): Promise<Boolean> => {
 }
 
 export const retrieveCategories = async (): Promise<ICategory[]> => {
-    const categories = await getArray<ICategory>("categories");
+    const categories = await getArray<ICategory>(CATEGORY_STORAGE_KEY);
     return categories;
 }
 
 export const loadOnLogin = async () => {
     try {
-        await store("locations", JSON.stringify(await getLocations()));
+        await store(LOCATIONS_STORAGE_KEY, JSON.stringify(await getLocations()));
     } catch (e) {
         console.error("Error loading login items", e);
     }
 }
 
 export const getCurrentSelectedLocation = async (): Promise<ILocation | undefined> => {
-    const locations = await getArrayOrEmpty<ILocation>("locations");
+    const locations = await getArrayOrEmpty<ILocation>(LOCATIONS_STORAGE_KEY);
     if (locations && locations.length === 0) {
         const location = await getLocation();
         return location
@@ -38,7 +39,7 @@ export const getCurrentSelectedLocation = async (): Promise<ILocation | undefine
 }
 
 export const retrieveLocations = async (): Promise<ILocation[]> => {
-    const locations = await getArrayOrEmpty<ILocation>("locations");
+    const locations = await getArrayOrEmpty<ILocation>(LOCATIONS_STORAGE_KEY);
     if (locations === null || locations === undefined || locations.length === 0) {
         locations.push(await getLocation());
         appendLocation(locations[0]);
@@ -54,7 +55,7 @@ export const appendLocation = async (location: ILocation) => {
         return;
     }
 
-    let locations: ILocation[] = await getArrayOrEmpty<ILocation>("locations");
+    let locations: ILocation[] = await getArrayOrEmpty<ILocation>(LOCATIONS_STORAGE_KEY);
     let index = -1;
 
     if (locations && locations.length > 0) {
@@ -65,5 +66,5 @@ export const appendLocation = async (location: ILocation) => {
     }
 
     locations = [location, ...locations];
-    await store("locations", JSON.stringify(locations));
+    await store(LOCATIONS_STORAGE_KEY, JSON.stringify(locations));
 }
