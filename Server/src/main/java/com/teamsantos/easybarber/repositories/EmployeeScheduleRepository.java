32967573e5f6738d81ad9c1f2135c1ac687680c6
Filipe Santos <filipesantosdev@gmail.com>
2024-07-12 18:33:05 +0100

    feat: Added Entities and repositories to handle Schedules

---
+++ Server/src/main/java/com/teamsantos/easybarber/repositories/EmployeeScheduleRepository.java
@@ -0,0 +1,10 @@
+package com.teamsantos.easybarber.repositories;
+
+import com.teamsantos.easybarber.entities.EmployeeSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeScheduleRepository extends JpaRepository<EmployeeSchedule, Long> {
+}
+
