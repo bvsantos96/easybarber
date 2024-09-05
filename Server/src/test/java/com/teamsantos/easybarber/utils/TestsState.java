80171ff64cb178c78179d0b36fc2285f5fbe4500
Filipe Santos <filipesantosdev@gmail.com>
2024-09-05 15:21:11 +0100

    dev

--- Server/src/test/java/com/teamsantos/easybarber/utils/TestsState.java
+++ Server/src/test/java/com/teamsantos/easybarber/utils/TestsState.java
@@ -5,35 +5,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestsState {
+    public static final String SERVICE_TYPES_CREATE_SERVICE_TYPES = "serviceTypesCreateServiceTypes";
+    public static final String SCHEDULE_CREATE_SCHEDULES = "scheduleCreateSchedules";
+    public static final String SCHEDULE_CREATE_EXCEPTIONS = "scheduleCreateExceptions";
+    public static final String SCHEDULE_DISABLE = "scheduleDisable";
+    public static final String ESTABLISHMENT_CREATE_ESTABLISHMENTS = "establishmentCreateEstablishments";
+    public static final String ESTABLISHMENT_TEST_EMPLOYEES = "establishmentTestEmployees";
+    public static final String ESTABLISHMENT_TEST_SERVICE = "establishmentTestService";
+    public static final String ESTABLISHMENT_ADD_IMAGES = "establishmentAddImages";
+    public static final String ESTABLISHMENT_DELETE_IMAGES = "establishmentDeleteImages";
+    public static final String EMPLOYEE_CREATE_EMPLOYEES = "employeeCreateEmployees";
+    public static final String EMPLOYEE_TEST_DELETE = "employeeTestDelete";
+    public static final String EMPLOYEE_CREATE_SERVICES = "employeeCreateServices";
+    public static final String EMPLOYEE_UPDATE_SERVICE = "employeeUpdateService";
+    public static final String EMPLOYEE_DELETE_IMAGES = "employeeDeleteImages";
+    public static final String EMPLOYEE_ADD_SERVICE_IMAGES = "employeeAddServiceImages";
+    public static final String EMPLOYEE_DELETE_SERVICE_IMAGES = "deleteServiceImages";
+    public static final String USER_TEST = "userTest";
+    public static final String APPOINTMENT_CREATE_APPOINTMENT = "appointmentCreateAppointment";
+    public static final String APPOINTMENT_CANCEL_APPOINTMENT = "appointmentCancelAppointment";
+    public static final String APPOINTMENT_CONFIRM_APPOINTMENT = "appointmentConfirmAppointment";
+    public static final String AUTH_TEST = "authTest";
+
     private static Set<String> completedSetups = Collections.synchronizedSet(new HashSet<>());
 
-    public static String SERVICE_TYPES_CREATE_SERVICE_TYPES = "serviceTypesCreateServiceTypes";
-    public static String SCHEDULE_CREATE_SCHEDULES = "scheduleCreateSchedules";
-    public static String SCHEDULE_CREATE_EXCEPTIONS = "scheduleCreateExceptions";
-    public static String SCHEDULE_DISABLE = "scheduleDisable";
-    public static String ESTABLISHMENT_CREATE_ESTABLISHMENTS = "establishmentCreateEstablishments";
-    public static String ESTABLISHMENT_TEST_EMPLOYEES = "establishmentTestEmployees";
-    public static String ESTABLISHMENT_TEST_SERVICE = "establishmentTestService";
-    public static String ESTABLISHMENT_ADD_IMAGES = "establishmentAddImages";
-    public static String ESTABLISHMENT_DELETE_IMAGES = "establishmentDeleteImages";
-    public static String EMPLOYEE_CREATE_EMPLOYEES = "employeeCreateEmployees";
-    public static String EMPLOYEE_TEST_DELETE = "employeeTestDelete";
-    public static String EMPLOYEE_CREATE_SERVICES = "employeeCreateServices";
-    public static String EMPLOYEE_UPDATE_SERVICE = "employeeUpdateService";
-    public static String EMPLOYEE_DELETE_IMAGES = "employeeDeleteImages";
-    public static String EMPLOYEE_ADD_SERVICE_IMAGES = "employeeAddServiceImages";
-    public static String EMPLOYEE_DELETE_SERVICE_IMAGES = "deleteServiceImages";
-    public static String USER_TEST = "userTest";
-    public static String APPOINTMENT_CREATE_APPOINTMENT = "appointmentCreateAppointment";
-    public static String APPOINTMENT_CANCEL_APPOINTMENT = "appointmentCancelAppointment";
-    public static String APPOINTMENT_CONFIRM_APPOINTMENT = "appointmentConfirmAppointment";
-    public static String AUTH_TEST = "authTest";
-
-    public static synchronized boolean ran(String setupName) {
+    public static boolean ran(String setupName) {
         return completedSetups.contains(setupName);
     }
 
-    public static synchronized void mark(String setupName) {
+    public static void mark(String setupName) {
         completedSetups.add(setupName);
     }
 }
