ea57d3c16f90a4d410ae55a7c34854477ce116e7
Filipe Santos <filipesantosdev@gmail.com>
2024-09-14 23:28:22 +0100

    feat: Added locaiton layout

--- Client/components/Button.tsx
+++ Client/components/Button.tsx
@@ -4,14 +4,14 @@ import Pressable from '../components/Pressable';
 import { getStyles } from '../styles/Button';
 import { useTheme } from '../styles/ThemeContext';
 
-export default function Button({ backgroundColor = "", buttonTextColor = "", borderColor = "", stylesInput = {}, onPress = () => { alert("onPress button function not passed in props") }, title = "No button tittle" }) {
+export default function Button({ backgroundColor = "", buttonTextColor = "", borderColor = "", borderRadius = 50, stylesInput = {}, onPress = () => { alert("onPress button function not passed in props") }, title = "No button tittle" }) {
     const theme = useTheme();
     const _backgroundColor = backgroundColor === "" ? theme.colors.mainColor : backgroundColor;
     const _borderColor = borderColor === "" ? theme.colors.mainColor : borderColor;
     const _buttonTextColor = buttonTextColor === "" ? theme.colors.backgroundColor : borderColor;
     const styles = getStyles();
     return (
-        <Pressable style={[styles.button, { backgroundColor: _backgroundColor, borderWidth: 1, borderColor: _borderColor }, stylesInput]} onPress={onPress}>
+        <Pressable style={[styles.button, { backgroundColor: _backgroundColor, borderWidth: 1, borderColor: _borderColor, borderRadius: borderRadius }, stylesInput]} onPress={onPress}>
             <Text style={[styles.textButton, { color: _buttonTextColor }]}>{title}</Text>
         </Pressable>
     );
