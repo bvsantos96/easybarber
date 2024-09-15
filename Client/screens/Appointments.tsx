a28f7300cd574dbae30d0a29556ec1cdb793c3be
Filipe Santos <filipesantosdev@gmail.com>
2024-09-15 17:32:33 +0100

    feat: New location functionality working. Modal used and can be triggered by setting flag from anywhere in the code; fix: Fixed button not working on onBoarding1

--- Client/screens/Appointments.tsx
+++ Client/screens/Appointments.tsx
@@ -1,10 +1,11 @@
-import { ScrollView, View } from 'react-native';
+import { Button, ScrollView, View } from 'react-native';
 import { getStyles } from '../styles/Appointments';
 import { useEffect, useState } from 'react';
 import { getAppointments } from '../utils/ApiRequest';
 import ListAppointments from '../components/ListAppointments';
 import React from 'react';
 import { Appointment } from '../declarations';
+import useLocationStore from '../storage/stores/LocationStore';
 
 export default function Home() {
     const styles = getStyles();
@@ -19,8 +20,13 @@ export default function Home() {
         fetchBarbers();
     }, []);
 
+    const {
+        setRequestingLocationPermission
+    } = useLocationStore();
+
     return (
         <View style={styles.container}>
+            <Button title="Book an appointment" onPress={() => { setRequestingLocationPermission(true) }} />
             <ScrollView contentContainerStyle={styles.listContainer}>
                 {appointmentList && appointmentList.map((appointment: Appointment) => {
                     return (
