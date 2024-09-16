b54092bfb5a35fc2d861f6e459d1d2a74e795364
Filipe Santos <filipesantosdev@gmail.com>
2024-09-16 17:35:21 +0100

    feat: Added swagger api

--- Server/src/main/java/com/teamsantos/easybarber/controllers/VersionController.java
+++ Server/src/main/java/com/teamsantos/easybarber/controllers/VersionController.java
@@ -2,11 +2,11 @@ package com.teamsantos.easybarber.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/version")
 public class VersionController {
     @Value("${app.data.version}")
