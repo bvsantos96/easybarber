7d686a64297ef6265c0f5352a4ec518a75e5585b
Filipe Santos <filipesantosdev@gmail.com>
2024-09-23 00:44:10 +0100

    feat: Appointment list Front-end

--- Client/components/ListAppointments.tsx
+++ Client/components/ListAppointments.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
-import { Appointment } from "../declarations";
+
+import { AppointmentInfo } from "../declarations";
 
 import { getStyles } from "../styles/Appointments";
+
 import ClockIcon from "@assets/icons/clock.svg";
 import Pressable from "../components/Pressable";
 
+import { getDateAsString, getTimeAsString } from "../utils/Utils";
 
-export default function ListAppointments({ appointment }: { appointment: Appointment }) {
+export default function ListAppointments({ appointment }: { appointment: AppointmentInfo }) {
+    const texts = require("@lang/en.json");
     const styles = getStyles();
+    const date = new Date(`${appointment.date}T${appointment.time}`);
+    const dateString = getDateAsString(date);
+    const timeString = getTimeAsString(date);
+
     return (
-        <Pressable onPress={() => { alert(`Open barber ${appointment.name}`); }} style={[styles.itemContainer, styles.shadow]}>
+        <Pressable onPress={() => { alert(`Open barber ${appointment.establishmentName}`); }} style={[styles.itemContainer, styles.shadow]}>
             <View style={styles.imageContainer}>
                 <Image source={{ uri: appointment.photo }} style={styles.imageStyle} />
             </View>
             <View style={styles.textContainer}>
-                <Text style={styles.title}>{appointment.name}</Text>
+                <Text numberOfLines={1} style={styles.title}>{appointment.establishmentName}</Text>
+                <View style={styles.subTitleContainer}>
+                    <Text numberOfLines={1} style={styles.locationText}>{appointment.entityName}</Text>
+                </View>
                 <View style={styles.infoContainer}>
                     <View style={styles.locationContainer}>
                         <ClockIcon width={styles.locationIcon.width} height={styles.locationIcon.height} style={styles.locationIcon} />
                     </View>
-                    <Text style={styles.locationText}>{appointment.from} to {appointment.to}</Text>
+                    <Text style={styles.locationText}>{dateString} {texts.at} {timeString}</Text>
                 </View>
             </View>
-        </Pressable>
+        </Pressable >
     );
 }
