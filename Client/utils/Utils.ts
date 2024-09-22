9ade8d2d43923a4e79ac1bf14235d09ba45a78f0
Filipe Santos <filipesantosdev@gmail.com>
2024-09-23 00:44:10 +0100

    feat: Appointment list Front-end

---
+++ Client/utils/Utils.ts
@@ -0,0 +1,23 @@
+export const getDateTimeAsString = (date: Date) => {
+    return `${twoDigits(date.getDate())}/${twoDigits(date.getMonth() + 1)} ${twoDigits(date.getHours())}:${twoDigits(date.getMinutes())}`;
+}
+
+export const getTimeAsString = (date: Date) => {
+    return `${twoDigits(date.getHours())}:${twoDigits(date.getMinutes())}`;
+}
+
+export const getFullDateAsString = (date: Date) => {
+    return `${twoDigits(date.getDate())}/${twoDigits(date.getMonth() + 1)}/${date.getFullYear()}`;
+}
+
+export const getDateAsString = (date: Date) => {
+    return `${twoDigits(date.getDate())}/${twoDigits(date.getMonth() + 1)}`;
+}
+
+export const twoDigits = (value: number) => {
+    return minDigits(value, 2);
+}
+
+export const minDigits = (value: number, digits: number) => {
+    return value.toString().padStart(digits, '0');
+}
