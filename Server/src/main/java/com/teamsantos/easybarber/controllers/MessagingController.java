97e1b7365a1cd7003ef66a872696a0e2cf863244
bvsantos96 <bvsantosdev@gmail.com>
2024-09-22 19:48:21 +0100

    updated controller tag for swagger

--- Server/src/main/java/com/teamsantos/easybarber/controllers/MessagingController.java
+++ Server/src/main/java/com/teamsantos/easybarber/controllers/MessagingController.java
@@ -8,14 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import com.teamsantos.easybarber.DTO.SmsDTO;
 
-@Controller
+@RestController
 public class MessagingController {
 
     private static final Logger logger = LoggerFactory.getLogger(MessagingController.class);
