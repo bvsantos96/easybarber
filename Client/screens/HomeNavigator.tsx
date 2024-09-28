ca6056b5bb037778da625dd1003294f6fde05905
Filipe Santos <filipesantosdev@gmail.com>
2024-09-28 19:23:23 +0100

    feat: HomeNavigator now has new Header

--- Client/screens/HomeNavigator.tsx
+++ Client/screens/HomeNavigator.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Entypo from '@expo/vector-icons/Entypo';
+
 import Home from './Home';
 import EstablishmentDetails from './EstablishmentDetails';
-import { View } from 'react-native';
 import { getStyles } from '../styles/HomeNavigator';
+
 
 export default function HomeNavigator() {
     const texts = require('@lang/en.json');
@@ -12,10 +15,26 @@ export default function HomeNavigator() {
     return (
         <View style={styles.container}>
             <Stack.Navigator initialRouteName={texts.tabs.back} >
-                <Stack.Screen name={texts.tabs.back} options={{ headerShown: false }} >
+                <Stack.Screen name={texts.tabs.back} options={{
+                    headerShown: false
+                }} >
                     {props => <Home {...props} />}
                 </Stack.Screen>
-                <Stack.Screen name={texts.tabs.establishmentDetails} component={EstablishmentDetails} />
+                <Stack.Screen
+                    options={{
+                        header: ({ navigation }) => (
+                            <View style={styles.header}>
+                                <View style={styles.headerContainer}>
+                                    <TouchableOpacity style={styles.goBack} onPress={() => navigation.goBack()}>
+                                        <Entypo name="chevron-small-left" size={styles.goBackIcon.width} color="black" />
+                                    </TouchableOpacity>
+                                    <Text style={styles.headerTitle}>{texts.tabs.establishmentDetails}</Text>
+                                    <View style={styles.headerFiller} />
+                                </View>
+                            </View>
+                        ),
+                    }}
+                    name={texts.tabs.establishmentDetails} component={EstablishmentDetails} />
             </Stack.Navigator>
         </View>
     );
