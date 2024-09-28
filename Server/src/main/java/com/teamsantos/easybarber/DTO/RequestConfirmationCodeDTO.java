8fb1265b82659380587c18d3f08170744641f465
bvsantos96 <bvsantosdev@gmail.com>
2024-09-28 01:27:23 +0100

    feat: pwd endpoint and improves do dtos

---
+++ Server/src/main/java/com/teamsantos/easybarber/DTO/RequestConfirmationCodeDTO.java
@@ -0,0 +1,10 @@
+package com.teamsantos.easybarber.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestConfirmationCodeDTO {
+    private String phoneNr;
+}
