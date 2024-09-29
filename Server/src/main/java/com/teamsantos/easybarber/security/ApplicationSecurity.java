165763cfc607eeec9cb87fd6a260d6f28ba99eee
bvsantos96 <bvsantosdev@gmail.com>
2024-09-29 15:31:40 +0100

    testing cors

--- Server/src/main/java/com/teamsantos/easybarber/security/ApplicationSecurity.java
+++ Server/src/main/java/com/teamsantos/easybarber/security/ApplicationSecurity.java
@@ -84,10 +84,10 @@ public class ApplicationSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://yourfrontend.com"));
+        configuration.setAllowedOrigins(Arrays.asList("89.114.65.230:3000", "89.114.65.230"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        //configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L); // 1 hour
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
