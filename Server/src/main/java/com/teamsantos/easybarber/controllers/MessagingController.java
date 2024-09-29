99512781c187c5045910ea2db00b6f41230dd4e2
bvsantos96 <bvsantosdev@gmail.com>
2024-09-29 02:19:32 +0100

    testing pipeline

--- Server/src/main/java/com/teamsantos/easybarber/controllers/MessagingController.java
+++ Server/src/main/java/com/teamsantos/easybarber/controllers/MessagingController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class MessagingController {
     @Autowired
     public MessagingController(MessagingService messagingService) {
         this.messagingService = messagingService;
+    }
+
+    @GetMapping("/sms/test")
+    public ResponseEntity<String> test(@RequestBody RequestConfirmationCode sms) {
+            return new ResponseEntity<>("it worked eheh", HttpStatus.OK);
     }
 
     @PostMapping("/sms/confirmation")
