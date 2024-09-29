a35c2999f714c2e64f92c36add939fc83ce5e447
bvsantos96 <bvsantosdev@gmail.com>
2024-09-29 03:17:29 +0100

    reverting pipeline test

--- Server/src/main/java/com/teamsantos/easybarber/services/MessagingService.java
+++ Server/src/main/java/com/teamsantos/easybarber/services/MessagingService.java
@@ -50,7 +50,7 @@ public class MessagingService {
     }
 
     public String pwdRecoveryCodeBodyMessage(String code) {
-        return "Ola. Your verification code is: " + code+". Use this code to reset your password. If you didn't request this, please ignore this message.";
+        return "Your verification code is: " + code+". Use this code to reset your password. If you didn't request this, please ignore this message.";
     }
 
     public void sendMessage(String to, String body) throws Exception {
