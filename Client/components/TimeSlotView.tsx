c4f9ec2c4b63e5fae3d54f0090d5364a3184d761
Filipe Santos <filipesantosdev@gmail.com>
2024-09-30 02:28:27 +0100

    feat: timeslots pageable

---
+++ Client/components/TimeSlotView.tsx
@@ -0,0 +1,31 @@
+import { View } from "react-native";
+import TimeSlot from "./TimeSlot";
+import { getStyles } from "@styles/TimeSlotView";
+
+export interface TimeSlotProps {
+    slot: TimeSlot,
+    select: (s: TimeSlot) => void,
+    selected: boolean
+}
+
+interface Props {
+    select: (s: TimeSlot) => void,
+    selected: TimeSlot | null,
+    slots: TimeSlot[][],
+    offset: number;
+}
+
+export default function TimeSlotView({ slots = [], select, selected, offset }: Props) {
+    const styles = getStyles();
+    return (
+        <View style={styles.container}>
+            {slots.map((line, index) => (
+                <View style={styles.line}>
+                    {line.map((slot, j) => (
+                        <TimeSlot key={offset + index * line.length + j} slot={slot} selected={selected === slot} select={select} />
+                    ))}
+                </View>
+            ))}
+        </View>
+    );
+}
