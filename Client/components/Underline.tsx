8ed875f595a577d2c77925ad772cecfeb1362492
Filipe Santos <filipesantosdev@gmail.com>
2024-09-30 01:08:32 +0100

    feat: Added calendar

--- Client/components/Underline.tsx
+++ Client/components/Underline.tsx
@@ -5,6 +5,8 @@ import { useTheme } from '../styles/ThemeContext';
 export const Underline = () => {
     const theme = useTheme();
     return (
-        <View style={[{ width: 45 * theme.dimensions.absoluteWidth, height: 2.5 * theme.dimensions.absoluteHeight, borderRadius: 5 }, { backgroundColor: theme.colors.mainColor }]} />
+        <View style={{ flexDirection: 'row' }}>
+            <View style={[{ flex: 0.85, height: 2.5 * theme.dimensions.absoluteHeight, borderRadius: 5 }, { backgroundColor: theme.colors.mainColor }]} />
+        </View>
     );
 }
