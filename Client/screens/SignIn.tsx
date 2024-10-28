8395302eb9c42cb699d016873a9b28943a9ded92
Filipe Santos <filipesantosdev@gmail.com>
2024-10-28 01:25:15 +0000

    feat: KeyboardAvoidingScrollView

--- Client/screens/SignIn.tsx
+++ Client/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Animated } from 'react-native';
 import LogoSmall from '../assets/images/logo.svg';
 import Login from '../components/Login';
@@ -7,33 +7,46 @@ import Register from '../components/Register';
 import { getStyles } from '../styles/Sign';
 import { useRef, useState } from 'react';
 import { NavigationProp } from '@react-navigation/native';
+import { useTheme } from '@styles/ThemeContext';
 
 export type SignInProps = {
     navigation: NavigationProp<any, any>,
     toggleNewUser?: () => void,
+    expand?: () => void,
+    collapse?: () => void,
 }
 
-export type Props = {
-    navigation: NavigationProp<any, any>,
-    toggleNewUser?: () => void
-}
-
-export default function SignIn({ navigation }: Props) {
+export default function SignIn({ navigation }: SignInProps) {
+    const theme = useTheme();
     const styles = getStyles();
     const [newUser, setNewUser] = useState(false);
     const translateYAnimation = useRef(new Animated.Value(0)).current;
+    const [expanded, setExpanded] = useState(false);
+    const heightAnim = useRef(new Animated.Value(styles.bottomTabContainer.height)).current;
+
+    const maxHeight = theme.dimensions.heightWithoutStatusBar;
+
+    useEffect(() => { adjustHeight() }, [expanded]);
+
+    const adjustHeight = () => {
+        Animated.timing(heightAnim, {
+            toValue: expanded ? maxHeight : styles.bottomTabContainer.height,
+            duration: 400,
+            useNativeDriver: false,
+        }).start();
+    }
 
     const changeNewUser = () => {
         Animated.timing(translateYAnimation, {
             toValue: 1,
             duration: 400,
-            useNativeDriver: true,
+            useNativeDriver: false,
         }).start(() => {
             setNewUser(!newUser)
             Animated.timing(translateYAnimation, {
                 toValue: 0,
                 duration: 400,
-                useNativeDriver: true,
+                useNativeDriver: false,
             }).start();
         });
     }
@@ -45,16 +58,37 @@ export default function SignIn({ navigation }: Props) {
             </View>
             <Animated.View style={[styles.bottomTabContainer,
             {
+                height: heightAnim,
+            },
+            {
                 transform: [{
                     translateY: translateYAnimation.interpolate({
                         inputRange: [0, 1],
-                        outputRange: [0, styles.bottomTabContainer.height],
+                        outputRange: [0, theme.dimensions.height],
                     })
                 }],
             }]}>
                 {!newUser ?
-                    <Login toggleNewUser={changeNewUser} navigation={navigation} /> :
-                    <Register toggleNewUser={changeNewUser} navigation={navigation} />
+                    <Login
+                        expand={() => {
+                            setExpanded(true);
+                        }}
+                        collapse={() => {
+                            setExpanded(false);
+                        }}
+                        toggleNewUser={changeNewUser}
+                        navigation={navigation}
+                    /> :
+                    <Register
+                        expand={() => {
+                            setExpanded(true);
+                        }}
+                        collapse={() => {
+                            setExpanded(false);
+                        }}
+                        toggleNewUser={changeNewUser}
+                        navigation={navigation}
+                    />
                 }
             </Animated.View>
         </View >
