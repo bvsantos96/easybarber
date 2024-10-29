6669d6f192e51888a159de8ffa65fa5661dd95f0
Filipe Santos <filipesantosdev@gmail.com>
2024-10-29 01:16:01 +0000

    feat: Added establishmentId to list of appointments

--- Server/src/main/java/com/teamsantos/easybarber/repositories/AppointmentRepository.java
+++ Server/src/main/java/com/teamsantos/easybarber/repositories/AppointmentRepository.java
@@ -65,6 +65,7 @@ public interface AppointmentRepository
                     s.id,
                     s.service.service.name,
                     s.employee.user.name,
+                    s.establishment.id,
                     s.establishment.name,
                     s.establishment.location,
                     s.establishment.address,
@@ -98,6 +99,7 @@ public interface AppointmentRepository
                     s.id,
                     s.service.service.name,
                     COALESCE(s.nonRegisteredUser, s.user.name),
+                    s.establishment.id
                     s.establishment.name,
                     s.establishment.location,
                     s.establishment.address,
