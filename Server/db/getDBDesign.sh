719bce10c18085f4fbc2424b4984f99fa0c7b1af
Filipe Santos <filipesantosdev@gmail.com>
2024-11-23 00:19:55 +0000

    feat: Cleaned up db related files, like dumps and schema.sql. (Created automatic way to retrieve the schema and to replace a db by another, since we have a stagging db generator we can replace the stagging one with that if for some reason we break the live one)

--- Server/db/getDBDesign.sh
+++ Server/db/getDBDesign.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Helper
+CURRENT_DATE=$(date +"%Y%m%dT%H%M%S")
+
 # Update these to your actual values
 DB_HOST="172.233.245.119"
 DB_PORT="3306"
 DB_USER="teamsantos"
 DB_PASSWORD="PMnLZYu4jVfT-U2" 
-DB_NAME_TEST="easy_barber_testing"
-DUMP_FILE="easy_barber_test_schema.sql"
+DB_NAME_TEST="easy_barber_stagging_heavy"
+DUMP_FILE="./schemas/schema_${CURRENT_DATE}.sql"
 
 # Create SQL dump file
 mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME_TEST > $DUMP_FILE --no-data
