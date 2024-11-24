7f63687c48428a4ef4a8dd0a23109e5169f4f4de
Filipe Santos <filipesantosdev@gmail.com>
2024-11-24 02:02:12 +0000

    dev: WIP

--- Client/screens/Settings.tsx
+++ Client/screens/Settings.tsx
@@ -2,13 +2,15 @@ import Pressable from "@components/Pressable";
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
+import SecurityIcon from "@icons/security.svg";
 import texts from "@lang/en.json";
 import { getStyles } from "@styles/Settings";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { getToken } from "utils/ApiRequest";
+import { getMobileCode, getMobileInformation, getToken } from "utils/ApiRequest";
 import { Routes } from "@navigation/Router";
 import { resetNavigation } from "utils/Utils";
 import useAuthStore from "storage/stores/AuthStore";
+import SettingItem from "@components/SettingItem";
 
 export default function Settings({ navigation }: PropNavigation) {
     const { toggleDoLogout } = useAuthStore();
@@ -33,12 +35,29 @@ export default function Settings({ navigation }: PropNavigation) {
                     <MaterialIcons name="login" width={styles.logOutIcon.width} height={styles.logOutIcon.height} color={styles.logOutIcon.color} size={styles.logOutIcon.width} />
                     <Text style={styles.logOutText}>{texts.login.signIn}</Text>
                 </Pressable>
-            </View >
+            </View>
         );
     }
 
     return (
         <View style={styles.container}>
+            <SettingItem
+                text={texts.settings.resetPwd}
+                icon={< SecurityIcon />}
+                onPress={async () => {
+                    const phoneInfo = await getMobileInformation();
+                    if (!phoneInfo) {
+                        return;
+                    }
+                    const mobileInformation = phoneInfo.countryCode + phoneInfo.phone;
+                    const _result = await getMobileCode(phoneInfo.countryCode, phoneInfo.phone);
+                    if (!_result) {
+                        return;
+                    }
+
+                    navigation.navigate(Routes.MobileConfirmation, { mobileInformation: mobileInformation, nextScreen: "ResetPwd", resetNavigationBoolean: false });
+                }}
+            />
             <Pressable style={styles.logOutContainer} onPress={() => {
                 toggleDoLogout();
             }}
