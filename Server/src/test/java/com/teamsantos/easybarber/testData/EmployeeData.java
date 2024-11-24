d6dea429c8f640328cfd6fa21f0545018b845af8
fmpisantos <39595188+fmpisantos@users.noreply.github.com>
2024-11-24 02:02:59 +0000

    feat: Fixed tests that were broken (#12)

--- Server/src/test/java/com/teamsantos/easybarber/testData/EmployeeData.java
+++ Server/src/test/java/com/teamsantos/easybarber/testData/EmployeeData.java
@@ -17,25 +17,25 @@ public class EmployeeData {
 
     static {
         employees = new ArrayList<>();
-        employees.add(new UserTestDTO(2L, "+351", "999999999", "Test123*", "Henrique"));
-        employees.add(new UserTestDTO(3L, "+351", "900000000", "Test123*", "Amigo do Joao"));
+        employees.add(new UserTestDTO(1L, "+351", "999999999", "Test123*", "Henrique"));
+        employees.add(new UserTestDTO(2L, "+351", "900000000", "Test123*", "Amigo do Joao"));
         employees.sort(Comparator.comparing(UserTestDTO::getId));
 
         employeesEstablishments = new TreeMap<>();
+        employeesEstablishments.put(1L, new ArrayList<>());
+        employeesEstablishments.get(1L).add(1L);
+        employeesEstablishments.get(1L).add(2L);
         employeesEstablishments.put(2L, new ArrayList<>());
-        employeesEstablishments.get(2L).add(1L);
         employeesEstablishments.get(2L).add(2L);
-        employeesEstablishments.put(3L, new ArrayList<>());
-        employeesEstablishments.get(3L).add(2L);
 
         adminEstablishments = new TreeMap<>();
+        adminEstablishments.put(1L, new ArrayList<>());
+        adminEstablishments.get(1L).add(1L);
         adminEstablishments.put(2L, new ArrayList<>());
-        adminEstablishments.get(2L).add(1L);
-        adminEstablishments.put(3L, new ArrayList<>());
-        adminEstablishments.get(3L).add(2L);
+        adminEstablishments.get(2L).add(2L);
 
         employeeImages = new TreeMap<>();
-        employeeImages.put(2L, new ArrayList<>() {
+        employeeImages.put(1L, new ArrayList<>() {
             {
                 add(new ImageDTO(null,
                         "https://d2zdpiztbgorvt.cloudfront.net/region1/us/807905/biz_photo/c2da6e290fa84b0392079ca2ae658f-pedro-barber-biz-photo-cf22ec40162841139be5358ccd8193-booksy.jpeg",
@@ -45,7 +45,7 @@ public class EmployeeData {
                         false));
             }
         });
-        employeeImages.put(3L, new ArrayList<>() {
+        employeeImages.put(2L, new ArrayList<>() {
             {
                 add(new ImageDTO(null,
                         "https://cdn.camberwellshopping.com.au/wp-content/uploads/2021/07/13111806/The-best-barbers-in-Camberwell.jpg",
