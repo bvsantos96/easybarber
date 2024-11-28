6c3e4749bf4981dcb2702e64c857a1ded9b69bc9
Filipe Santos <filipesantosdev@gmail.com>
2024-11-28 19:12:45 +0000

    fix: Removed console.log

--- Client/screens/ForgotPwd.tsx
+++ Client/screens/ForgotPwd.tsx
@@ -34,7 +34,6 @@ export default function ForgotPwd({ navigation }: PropNavigation) {
 
     const forgotPwd = async () => {
         const blockuntil = await getMobileCodeResetPwd(nation ? nation.callingCode[0] : "", phone);
-        console.log(blockuntil);
         if (!blockuntil) {
             return;
         }
