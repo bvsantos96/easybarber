e1ab1983170ac0862a111b1582708d14b969d397
bvsantos96 <119519648+bvsantos96@users.noreply.github.com>
2025-01-25 23:09:24 +0000

    implemented product screens (data currently static)

--- EmployeeClient/styles/Input.tsx
+++ EmployeeClient/styles/Input.tsx
@@ -5,8 +5,8 @@ export const getStyles = () => {
     const theme = useTheme();
     return StyleSheet.create({
         container: {
-            width: theme.dimensions.input.width,
-            height: theme.dimensions.input.height,
+            width: '100%',
+            height: '100%'
         },
         inputView: {
             width: '100%',
