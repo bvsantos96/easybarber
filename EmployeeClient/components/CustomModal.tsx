0db97243e280d23b4f770b657dae9a76ea946b69
fmpisantos <39595188+fmpisantos@users.noreply.github.com>
2025-01-26 16:46:23 +0000

    Filipe/employee schedule (#32)
    * feat: Added modal from bottom. And timesheets list.
    
    * feat: Self adjust list of timesheets when 2 collumns are needed
    
    * feat: Added generic slidding item. Used slidding item in timesheet for deletion
    
    * feat: Added monthly view of calendar
    
    * feat: Scrollable list now actually scrolls
    
    * feat: Removed unecessary center css
    
    * feat: Added time selection on set absence
    
    * feat: Added functionality to input
    
    * feat: Adjusted timesheet day nonSelected items background color

---
+++ EmployeeClient/components/CustomModal.tsx
@@ -0,0 +1,81 @@
+import React, { useImperativeHandle, useRef, useState } from 'react';
+import { View, StyleProp, ViewStyle } from 'react-native';
+import { BottomSheetModal, BottomSheetBackdrop, BottomSheetView } from '@gorhom/bottom-sheet/src';
+import { useSharedValue } from 'react-native-reanimated';
+import { useTheme } from '../styles/ThemeContext';
+import Pressable from './Pressable';
+
+type CustomModalProps = {
+    children?: React.ReactNode;
+    modalContent: React.ReactNode;
+    modalHeight: number;
+    buttonStyle?: StyleProp<ViewStyle>;
+    snapPoints?: number[];
+};
+
+export interface CustomModalRef {
+    toggleModal: () => void;
+}
+
+const CustomModal: React.ForwardRefRenderFunction<CustomModalRef, CustomModalProps> = (
+    { children, modalContent, modalHeight, buttonStyle, snapPoints },
+    ref
+) => {
+    const theme = useTheme();
+    const [isVisible, setIsVisible] = useState(false);
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+    const sharedVal = useSharedValue(0);
+    const modalBottomPadding = 20 * theme.dimensions.absoluteHeight;
+    const [modalContentHeight] = useState(modalHeight ? modalHeight + modalBottomPadding : 0);
+
+    useImperativeHandle(ref, () => ({
+        toggleModal,
+    }));
+
+    const toggleModal = () => {
+        if (isVisible) {
+            bottomSheetModalRef.current?.dismiss();
+        } else {
+            bottomSheetModalRef.current?.present();
+        }
+        setIsVisible(!isVisible);
+    };
+
+    const backdropProps = {
+        closeOnPress: true,
+        appearsOnIndex: 0,
+        disappearsOnIndex: -1,
+        enableTouchThrough: false,
+    };
+
+    return (
+        <>
+            {children && (
+                <Pressable style={buttonStyle} onPress={toggleModal}>
+                    {children}
+                </Pressable>
+            )}
+            <BottomSheetModal
+                ref={bottomSheetModalRef}
+                onDismiss={() => setIsVisible(false)}
+                snapPoints={snapPoints || [modalContentHeight + theme.dimensions.tabHeight, theme.dimensions.maxSnapPoint]}
+                backdropComponent={() => (
+                    <BottomSheetBackdrop
+                        style={{ backgroundColor: theme.colors.modalBackdrop, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+                        animatedIndex={sharedVal}
+                        animatedPosition={sharedVal}
+                        {...backdropProps}
+                    />
+                )}
+            >
+                <BottomSheetView>
+                    <View style={{ "minHeight": modalContentHeight }} >
+                        {modalContent}
+                    </View>
+                </BottomSheetView>
+            </BottomSheetModal>
+        </>
+    );
+};
+
+export default React.forwardRef(CustomModal);
