4f7bc7e0903ed1e072cc599ec3dd60eac5bf2ed3
Filipe Santos <filipesantosdev@gmail.com>
2025-02-24 21:31:53 +0000

    feat: Adjusted multicolumn list

--- EmployeeClient/styles/ServiceItem.tsx
+++ EmployeeClient/styles/ServiceItem.tsx
@@ -3,34 +3,26 @@ import { useTheme } from '@styles/ThemeContext';
 
 export const getStyles = () => {
     const theme = useTheme();
+    const containerWidth = 150 * theme.dimensions.absoluteWidth;
     return StyleSheet.create({
         container: {
-            width: 160*theme.dimensions.absoluteWidth,
-            height: 215 * theme.dimensions.absoluteHeight,
+            width: containerWidth,
             borderRadius: 15,
             backgroundColor: 'white',
             marginVertical: 10 * theme.dimensions.absoluteHeight,
-            alignSelf: 'center',
             alignItems: 'center'
         },
         image: {
-            top: 35 * theme.dimensions.absoluteHeight,
-            width: 129 * theme.dimensions.absoluteHeight,
-            height: 65 * theme.dimensions.absoluteHeight,
-            borderRadius: 15,
-            resizeMode: 'contain' //TODO: resizeMode is deprecated
+            width: containerWidth - 35 * theme.dimensions.absoluteWidth,
+            height: 85 * theme.dimensions.absoluteHeight,
+            objectFit: 'cover',
+            borderRadius: 10,
         },
         textContainer: {
-            position: 'absolute',
-            top: 115 * theme.dimensions.absoluteHeight,
-            left: 15 * theme.dimensions.absoluteWidth,
-            width: 130 * theme.dimensions.absoluteWidth,
-            height: 90 * theme.dimensions.absoluteHeight
+            width: containerWidth - 35 * theme.dimensions.absoluteWidth,
+            alignItems: 'flex-start',
         },
         brandText: {
-            position: 'absolute',
-            width: 130 * theme.dimensions.absoluteWidth,
-            height: 25 * theme.dimensions.absoluteHeight,
             fontFamily: 'Poppins',
             fontWeight: '500',
             fontSize: 16 * theme.dimensions.absoluteHeight,
@@ -39,22 +31,15 @@ export const getStyles = () => {
             textAlign: 'left',
         },
         nameText: {
-            position: 'absolute',
-            width: 130 * theme.dimensions.absoluteWidth,
-            height: 32 * theme.dimensions.absoluteHeight,
-            top: 30 * theme.dimensions.absoluteHeight,
             fontFamily: 'Poppins',
             fontWeight: '400',
             fontSize: 10 * theme.dimensions.absoluteHeight,
             lineHeight: 16 * theme.dimensions.absoluteHeight,
+            minHeight: 32 * theme.dimensions.absoluteHeight,
             letterSpacing: 0,
             textAlign: 'left',
         },
         priceText: {
-            position: 'absolute',
-            width: 130 * theme.dimensions.absoluteWidth,
-            height: 21 * theme.dimensions.absoluteHeight,
-            top: 70 * theme.dimensions.absoluteHeight,
             fontFamily: 'Poppins',
             fontWeight: '500',
             fontSize: 14 * theme.dimensions.absoluteHeight,
@@ -62,12 +47,12 @@ export const getStyles = () => {
             letterSpacing: 0,
             textAlign: 'left',
         },
-        editIcon:{
+        editIcon: {
             position: 'absolute',
             top: 4 * theme.dimensions.absoluteHeight,
             left: 125 * theme.dimensions.absoluteWidth,
-            width: 20 * theme.dimensions.absoluteWidth,
-            height: 20 * theme.dimensions.absoluteWidth,
-        }  
+            width: 30 * theme.dimensions.absoluteWidth,
+            height: 30 * theme.dimensions.absoluteWidth,
+        }
     })
 }
